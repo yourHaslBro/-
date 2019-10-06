20ec8f07c97be13dd4a4b3182287f7936f690258
yourHaslBro <poyta.n.o@gmail.com>
2019-10-06 20:23:02 +0300

    v 1.0

---
+++ Компонент иерархического меню с примером/structure.js
@@ -0,0 +1,57 @@
+﻿'use strict';
+
+const comp = `<div>
+  <a v-if="_.hasIn(structure, 'a')" :href='structure.a'>
+    <div
+      :class="[
+        'left-border-title-structure',
+        'title-size-structure',
+        structure.color + '-border-title-structure'
+      ]"
+    > {{ structure.title }} </div>
+  </a>
+  <div v-else>
+    <div>
+      <div
+        list
+        :list-open="String(show)"
+        :class="[
+          'left-border-title-structure',
+          'title-size-structure',
+          structure.color + '-border-title-structure'
+        ]"
+        @click="show = !show"
+      > {{ structure.title }} </div>
+    </div>
+    <div class="options-container-structure" v-if="show">
+      <div v-for="r in structure.array">
+        <a v-if="_.hasIn(r, 'val')" :href="r.a">
+          <div
+            :class="[
+              'left-border-option-structure',
+              'option-size-structure',
+              structure.color + '-border-option-structure'
+            ]"
+          > {{ r.val }} </div>
+        </a>
+        <div v-else>
+          <structure-menu :structure="r"></structure-menu>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`;
+
+
+
+const structureMenu = {
+  props: {
+    structure: {
+      required: true,
+      type: Object
+    }
+  },
+  template: comp,
+  name: 'structure-menu',
+  data: () => ({show: false})
+};
